f6bbe54f33bb49d29b13dd3b527546a97928e371
Alok kumar <72178871+alok9038@users.noreply.github.com>
2021-01-14 13:16:11 +0530

    student work

---
+++ student/include/sidebar.php
@@ -0,0 +1,17 @@
+<div class="bg-dark p-0 student-sidebar d-none d-lg-block" style="min-height:90vh;height:auto;width:20%;">
+                    <div class="profile">
+                        <div class="dp bg-danger mx-auto mt-4 rounded-circle" style="width:70px; height:70px;"></div>
+                        <!-- <div class="coll d-flex container">
+                            <div class="b1"><i class="fa fa-user text-white"></i></div>
+                            <div class="b1"><i class="fa fa-power-off text-white"></i></div>
+                        </div> -->
+                    </div>
+                <div class="list-group mt-5">
+                    
+                    <a href="" class="list-group-item rounded-0 list-group-item-action text-light border-0 p-3 list">Dashboard</a>
+                    <a href="" class="list-group-item rounded-0 list-group-item-action text-light border-0 p-3 list">Courses</a>
+                    <a href="" class="list-group-item rounded-0 list-group-item-action text-light border-0 p-3 list">Payments</a>
+                    <a href="" class="list-group-item rounded-0 list-group-item-action text-light border-0 p-3 list">Profile</a>
+                    <a href="" class="list-group-item rounded-0 list-group-item-action text-light border-0 p-3 list"><i class="fa text-danger fa-power-off"></i> Logout</a>
+                </div>
+            </div>
--- student/mycourse.php
+++ student/mycourse.php
@@ -1,1 +1,24 @@
  
+ 
+<?php include('include/header.php'); ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <?php include('include/sidebar.php'); ?>
+        <div class="p-5 content2">
+            
+            <div class="container mt-5">
+                <table class="table table-borderless table-md table-sm table-responsive">
+                    <tr class="bg-dark text-white">
+                        <th class="py-2">Sr No.</th>
+                        <th class="py-2">Logo</th>
+                        <th class="py-2">Course Title</th>
+                        <th class="py-2">Duration</th>
+                        <th class="py-2"></th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('include/footer.php'); ?>
