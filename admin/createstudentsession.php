fc68f5baf86eb153edbc7b4eba51d5caa81a5d0f
vikas <vikas.vm788@gmail.com>
2021-01-17 14:39:06 +0530

    2:38

---
+++ admin/createstudentsession.php
@@ -0,0 +1,7 @@
+<?php include 'header.php';
+$id = $_GET['id'];
+$student = callingRecord('students', "id='$id'");
+echo $username = $student['email'];
+$_SESSION['student']=$username;
+echo $_SESSION['student'];
+redirect('../student/index');
